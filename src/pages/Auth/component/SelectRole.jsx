7619ef2b7fa7138ed003dc13baa22697b6acb71d
vuongbachdoan <vuongbachdoan@gmail.com>
2023-10-16 01:05:59 +0700

    fix: fix dark mode in auth(select role)

--- src/pages/Auth/component/SelectRole.jsx
+++ src/pages/Auth/component/SelectRole.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Image, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Image, Text, useColorModeValue } from "@chakra-ui/react";
 import Student1 from '../../../assets/images/student1.png';
 import Lecture1 from '../../../assets/images/lecture1.png';
 import DecorRoleOrange from '../../../assets/images/decor_role_orange.png'
@@ -7,85 +7,84 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 export const SelectRole = () => {
+    const bg = useColorModeValue('#FFF', 'gray.700');
     const navigate = useNavigate();
     const [isStudentSelected, setIsStudentSelected] = React.useState(true);
 
     return (
-        <>
+        <Flex
+            flexDirection='column'
+            alignItems='center'
+        >
+            <Text marginBottom={30} fontWeight='semibold' fontSize='2xl'>Which role is similar to you ?</Text>
+
             <Flex
-                flexDirection='column'
-                alignItems='center'
+                flexDirection='row'
+                columnGap={30}
             >
-                <Text marginBottom={30} fontWeight='semibold' fontSize='2xl'>Which role is similar to you ?</Text>
-
-                <Flex
-                    flexDirection='row'
-                    columnGap={30}
+                <Box
+                    bg={bg}
+                    borderWidth={0.5}
+                    borderColor={isStudentSelected ? '#FF8F46' : 'transparent'}
+                    opacity={isStudentSelected ? 1 : 0.5}
+                    borderRadius={28}
+                    width='fit-content'
+                    height='fit-content'
+                    cursor='pointer'
+                    onClick={() => setIsStudentSelected(true)}
                 >
-                    <Box
-                        backgroundColor='#FFF'
-                        borderWidth={0.5}
-                        borderColor={isStudentSelected ? '#FF8F46' : 'transparent'}
-                        opacity={isStudentSelected ? 1 : 0.5}
-                        borderRadius={28}
-                        width='fit-content'
-                        height='fit-content'
-                        cursor='pointer'
-                        onClick={() => setIsStudentSelected(true)}
+                    <Flex
+                        justifyContent='center'
+                        alignItems='center'
+                        position='relative'
+                        margin='20px'
+                        width={250}
+                        height={250}
+                        flexDirection='column'
                     >
-                        <Flex
-                            justifyContent='center'
-                            alignItems='center'
-                            position='relative'
-                            margin='20px'
-                            width={250}
-                            height={250}
-                            flexDirection='column'
-                        >
-                            <Image src={DecorRoleOrange}
-                                position='absolute'
-                                top={0}
-                                left={0}
-                                width='100%'
-                            />
-                            <Image src={Student1} width={100} height={100} borderRadius={14} zIndex={10} />
-                            <Text marginTop={3} fontWeight='semibold' fontSize='md'>I am Student</Text>
-                        </Flex>
-                    </Box>
-                    <Box
-                        backgroundColor='#FFF'
-                        borderWidth={0.5}
-                        borderColor={!isStudentSelected ? '#45CE7B' : 'transparent'}
-                        opacity={!isStudentSelected ? 1 : 0.5}
-                        borderRadius={28}
-                        width='fit-content'
-                        height='fit-content'
-                        cursor='pointer'
-                        onClick={() => setIsStudentSelected(false)}
+                        <Image src={DecorRoleOrange}
+                            position='absolute'
+                            top={0}
+                            left={0}
+                            width='100%'
+                        />
+                        <Image src={Student1} width={100} height={100} borderRadius={14} zIndex={10} />
+                        <Text marginTop={3} fontWeight='semibold' fontSize='md'>I am Student</Text>
+                    </Flex>
+                </Box>
+                <Box
+                    bg={bg}
+                    borderWidth={0.5}
+                    borderColor={!isStudentSelected ? '#45CE7B' : 'transparent'}
+                    opacity={!isStudentSelected ? 1 : 0.5}
+                    borderRadius={28}
+                    width='fit-content'
+                    height='fit-content'
+                    cursor='pointer'
+                    onClick={() => setIsStudentSelected(false)}
+                >
+                    <Flex
+                        justifyContent='center'
+                        alignItems='center'
+                        position='relative'
+                        margin='20px'
+                        width={250}
+                        height={250}
+                        flexDirection='column'
                     >
-                        <Flex
-                            justifyContent='center'
-                            alignItems='center'
-                            position='relative'
-                            margin='20px'
-                            width={250}
-                            height={250}
-                            flexDirection='column'
-                        >
-                            <Image src={DecorRoleGreen}
-                                position='absolute'
-                                top={0}
-                                left={0}
-                                width='100%'
-                            />
-                            <Image src={Lecture1} width={100} height={100} borderRadius={14} zIndex={10} />
-                            <Text marginTop={3} fontWeight='semibold' fontSize='md'>I am Lecture</Text>
-                        </Flex>
-                    </Box>
-                </Flex>
-
-                <Button onClick={() => navigate('login')} marginTop={30} borderRadius={15} size='lg' backgroundColor={isStudentSelected ? "#FF8F46" : '#45CE7B'} _hover={{backgroundColor: isStudentSelected ? '#E86C1C' : '#20B65B'}}>Next</Button>
+                        <Image src={DecorRoleGreen}
+                            position='absolute'
+                            top={0}
+                            left={0}
+                            width='100%'
+                        />
+                        <Image src={Lecture1} width={100} height={100} borderRadius={14} zIndex={10} />
+                        <Text marginTop={3} fontWeight='semibold' fontSize='md'>I am Lecture</Text>
+                    </Flex>
+                </Box>
             </Flex>
-        </>
+
+            <Button onClick={() => navigate('login')} marginTop={30} borderRadius={15} size='lg' backgroundColor={isStudentSelected ? "#FF8F46" : '#45CE7B'} _hover={{ backgroundColor: isStudentSelected ? '#E86C1C' : '#20B65B' }}>Next</Button>
+        </Flex>
     );
 }
