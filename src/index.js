afeac535891b4bdcd05c8b9a2d9924fb50f5fb99
vuongbachdoan <vuongbachdoan@gmail.com>
2023-10-24 16:16:59 +0700

    fix: fix aws-export not exist

--- src/index.js
+++ src/index.js
@@ -5,9 +5,8 @@ import App from './App';
 import * as serviceWorker from './config/serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
+const awsExports = JSON.parse(process.env.REACT_APP_AMPLIFY_CREDENTIALS)
 Amplify.configure(awsExports);
-// Amplify.configure(JSON.parse(process.env.REACT_APP_AMPLIFY_CREDENTIALS));
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
