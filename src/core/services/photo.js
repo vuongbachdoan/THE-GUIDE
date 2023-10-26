3965f67915064fa306cdbb4947ca52f8da803256
vuongbachdoan <vuongbachdoan@gmail.com>
2023-10-26 10:33:09 +0700

    feat: add post api

--- src/core/services/photo.js
+++ src/core/services/photo.js
@@ -14,3 +14,18 @@ export const updateUserAvatar = async (userId, image, type) => {
 
     return response.data;
 }
+
+export const updatePostCover = async (userId, image, type) => {
+    const data = {
+        image: image, // base64
+        type: type
+    };
+
+    const response = await axios.post(`https://idc43jsr1c.execute-api.us-east-1.amazonaws.com/posts/${userId}/cover`, data, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    return response.data;
+}
---
+++ src/core/services/post.js
@@ -0,0 +1,60 @@
+import axios from 'axios';
+
+/**
+ * 
+ * @param {*} data 
+ * id: requestJSON.id,
+ * subjectCode: requestJSON.subjectCode,
+ * department: requestJSON.department,
+ * title: requestJSON.title,
+ * description: requestJSON.description,
+ * creatorId: requestJSON.creatorId,
+ * createAt: requestJSON.createAt,
+ * updatedAt: requestJSON.updatedAt,
+ * content: requestJSON.content,
+ * status: requestJSON.status,
+ * liked: requestJSON.liked,
+ * commentIds: requestJSON.commentIds,
+ * shared: requestJSON.shared,
+ * viewed: requestJSON.viewed
+ * @returns 
+ */
+export const createPost = async (data) => {
+    const response = await axios.post('https://idc43jsr1c.execute-api.us-east-1.amazonaws.com/posts', data, {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
+
+    return response.data;
+}
+
+export const getPosts = async () => {
+    const response = await axios.get('https://idc43jsr1c.execute-api.us-east-1.amazonaws.com/posts', {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    return response.data;
+}
+
+export const getPost = async (postId) => {
+    const response = await axios.get(`https://idc43jsr1c.execute-api.us-east-1.amazonaws.com/posts/${postId}`, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    return response.data;
+}
+
+export const updatePost = async (postData) => {
+    const response = await axios.patch(`https://idc43jsr1c.execute-api.us-east-1.amazonaws.com/posts/${postData.id}`, postData, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    return response.data;
+}
