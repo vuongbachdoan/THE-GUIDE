ef6a22c8bb84b531f59c8490c75fd8762ec08103
vuongbachdoan <vuongbachdoan@gmail.com>
2023-11-09 10:38:09 +0700

    feat: add video streaming

--- src/config/router/AppRouting.jsx
+++ src/config/router/AppRouting.jsx
@@ -21,7 +21,7 @@ import { AboutUs } from '../../pages/AboutUs';
 import { PanelPostDetail } from '../../pages/Home/components/PanelPostDetail';
 import { PanelEditPost } from '../../pages/Home/components/PanelEditPost';
 import { Livestream } from '../../pages/Livestream';
-import { LivestreamViewer } from '../../pages/Livestream/components/LivestreamViewer';
+import { LivestreamViewerJoin } from '../../pages/Livestream/components/LivestreamViewerJoin';
 
 export const AppRouting = () => {
     return (
@@ -49,7 +49,7 @@ export const AppRouting = () => {
             <Route path='/policy' element={<Policy />} />
             <Route path='/about-us' element={<AboutUs />} />
             <Route path='/livestream' element={< Livestream />} />
-            <Route path='/livestream/view' element={< LivestreamViewer />} />
+            <Route path='/livestream/view' element={< LivestreamViewerJoin />} />
             <Route path='*' element={<Error />} />
         </Routes>
     );
