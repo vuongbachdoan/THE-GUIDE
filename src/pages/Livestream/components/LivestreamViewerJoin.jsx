9363bd3544558dc901693bd4cd18989d91d6a32e
vuongbachdoan <vuongbachdoan@gmail.com>
2023-11-09 13:53:41 +0700

    feat: complete connect to AWS IVS

--- src/pages/Livestream/components/LivestreamViewerJoin.jsx
+++ src/pages/Livestream/components/LivestreamViewerJoin.jsx
@@ -1,98 +1,74 @@
-import { Button, Flex, Input } from '@chakra-ui/react';
-import React, { useState, useEffect, useRef } from 'react';
-const IVSPackage = window.IVSPlayer;
+import React, { useState, useRef } from "react";
+import { registerIVSQualityPlugin, registerIVSTech } from "amazon-ivs-player";
+import videojs from "video.js";
+import { Box, Button, Flex, Input, Spinner } from "@chakra-ui/react";
 
-const LivestreamViewer = ({ stream }) => {
-    const videoElement = useRef(null);
-    const player = useRef(null);
-
-    useEffect(() => {
-        if (!IVSPackage.isPlayerSupported) {
-            throw new Error('IVS Player is not supported in this browser');
-        }
-
-        player.current = IVSPackage.create();
-        player.current.attachHTMLVideoElement(videoElement.current);
-        attachListeners();
-
-        loadAndPlay(stream);
-
-        // Clean up on unmount
-        // return () => {
-        //     if (player.current) {
-        //         player.current.removeEventListener();
-        //         player.current = null;
-        //     }
-        // };
-    }, [stream]);
-
-    const loadAndPlay = (stream) => {
-        player.current.setAutoplay(true);
-        player.current.load(stream);
-    }
-
-    const attachListeners = () => {
-        const { ErrorType, PlayerEventType, PlayerState } = IVSPackage;
-    
-        for (let state of Object.values(PlayerState)) {
-            player.current.addEventListener(state, () => {
-                console.log(state);
-            });
-        }
-    
-        player.current.addEventListener(PlayerEventType.INITIALIZED, () => {
-            console.log('INITIALIZED');
-        });
-    
-        player.current.addEventListener(PlayerEventType.ERROR, (error) => {
-            const statusTooManyRequests = 429;
-            if (error.type === ErrorType.NOT_AVAILABLE && error.code === statusTooManyRequests) {
-                console.error('Concurrent-viewer limit reached', error);
-            } else {
-                console.error('ERROR', error);
-            }
-        });
-    
-        player.current.addEventListener(PlayerEventType.QUALITY_CHANGED, (quality) => {
-            console.log('QUALITY_CHANGED', quality);
-        });
-    
-        // This event fires when text cues are encountered, such as captions or subtitles
-        player.current.addEventListener(PlayerEventType.TEXT_CUE, (cue) => {
-            console.log('TEXT_CUE', cue.startTime, cue.text);
-        });
-    
-        // This event fires when embedded Timed Metadata is encountered
-        player.current.addEventListener(PlayerEventType.TEXT_METADATA_CUE, (cue) => {
-            console.log('Timed metadata', cue.text);
-        });
-    }
-
-    return (
-        <video id="video-player" ref={videoElement}/>
-    );
-}
+const DEFAULT_POSITION = "auto";
+const TRANSITION = "100ms ease-in-out";
 
 export const LivestreamViewerJoin = () => {
-    const [stream, setStream] = useState('https://0bf28f2401c9.us-east-1.playback.live-video.net/api/video/v1/us-east-1.955228589631.channel.v4c7evGHqxKq.m3u8');
-    const [showPlayer, setShowPlayer] = useState(false);
+    const divEl = React.useRef(null);
+    const videoEl = React.useRef(null);
+    const [isJoin, setIsJoin] = React.useState(false);
+    const [linkUrl, setLinkUrl] = React.useState('');
+    const [isLoading, setIsLoading] = React.useState(false);
 
-    const handleNextClick = () => {
-        setShowPlayer(true);
-    }
+    React.useEffect(() => {
+        if (isJoin && linkUrl) {
+            setIsLoading(true);
+            const script = document.createElement('script');
 
-    if (showPlayer) {
-        return <LivestreamViewer stream={stream} />;
-    }
+            script.src = 'https://player.live-video.net/1.0.0/amazon-ivs-player.min.js';
+            script.async = true;
+
+            document.body.appendChild(script);
+
+            script.onload = () => {
+                // eslint-disable-next-line no-undef
+                if (IVSPlayer.isPlayerSupported) {
+                    // eslint-disable-next-line no-undef
+                    const player = IVSPlayer.create();
+                    player.attachHTMLVideoElement(document.getElementById('video-player'));
+                    player.load(linkUrl);
+                    player.play();
+                    setIsLoading(false);
+                }
+            }
+
+            return () => {
+                document.body.removeChild(script);
+            }
+        }
+    }, [isJoin]);
+
 
     return (
-        <Flex
-            flexDirection='row'
-            columnGap={1}
-            width='100%'
-        >
-            <Input borderRadius={15} type="text" value={stream} onChange={e => setStream(e.target.value)} />
-            <Button backgroundColor='#FF8F46' _hover={{backgroundColor: '#FF8F46'}} borderRadius={15} onClick={handleNextClick}>Next</Button>
-        </Flex>
+        <>
+            {
+                isJoin ?
+                    <div ref={divEl}>
+                        <video
+                            id="video-player"
+                            ref={videoEl}
+                            playsInline
+                            autoPlay
+                            height={300}
+                            muted
+                        />
+                    </div>
+                    :
+                    <Flex
+                        flexDirection='row'
+                        columnGap={3}
+                        width='100%'
+                        margin={3}
+                        maxWidth={1024}
+                    >
+                        <Input value={linkUrl} onChange={(e) => setLinkUrl(e.target.value)} borderRadius={10} placeholder="Room URL" />
+                        <Button borderRadius={10} backgroundColor='#FF8F46' _hover={{backgroundColor: '#FF8F46'}} onClick={() => setIsJoin(true)}>{isLoading ? <Spinner/> : 'Join room'}</Button>
+                    </Flex>
+            }
+        </>
+
     );
-}
+};
